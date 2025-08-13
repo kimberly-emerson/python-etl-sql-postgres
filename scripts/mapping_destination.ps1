681a0f90e0903f67c99648bc8811c04e5534d7d1
kimberly-emerson <kimberly.emerson@outlook.com>
2025-08-13 17:58:25 -0500

    Update mapping powershell scripts and json files

--- scripts/mapping_destination.ps1
+++ scripts/mapping_destination.ps1
@@ -1,4 +1,4 @@
 $csvData = Import-Csv -Path "C:\demos\python-etl-sql-postgres\sql\mapping.csv"
-$csvData = $csvData | Where-Object Is_App_Table -eq 1 | Select-Object destination_query_create, destination_query_insert, execution_order | Sort-Object execution_order, destination_query_create
+$csvData = $csvData | Where-Object Is_App_Table -eq 1 | Select-Object destination_query_create, destination_query_insert, table_id, execution_order | Sort-Object execution_order, destination_query_insert
 $jsonData = $csvData | ConvertTo-Json
 $jsonData | Set-Content -Path "C:\demos\python-etl-sql-postgres\sql\mapping_destination.json"
--- scripts/mapping_source.ps1
+++ scripts/mapping_source.ps1
@@ -1,4 +1,4 @@
 $csvData = Import-Csv -Path "C:\demos\python-etl-sql-postgres\sql\mapping.csv"
-$csvData = $csvData | Where-Object Is_App_Table -eq 1 | Select-Object source_query_select, execution_order | Sort-Object execution_order, source_query_select
+$csvData = $csvData | Where-Object Is_App_Table -eq 1 | Select-Object source_query_select, table_id, execution_order | Sort-Object execution_order, source_query_select
 $jsonData = $csvData | ConvertTo-Json
 $jsonData | Set-Content -Path "C:\demos\python-etl-sql-postgres\sql\mapping_source.json"
