54f81681faa41508be01b5edbfbfcca4443b23dc
kimberly-emerson <kimberly.emerson@outlook.com>
2025-08-14 17:53:21 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,23 +5,16 @@ tba
 import os
 import logging
 from db.postgresql import build_pg_database
+import utils.logger
 from build.create_destination_tables import create_pg_tables
 from build.truncate_destination_tables import truncate_pg_tables
 from extract.get_source_data import get_source_data
 from load.insert_source_data import insert_pg_tables
     
-LOGS_PATH = os.getenv("LOGS_PATH")
 SQL_PATH = os.getenv('SQL_PATH')
 SOURCE_FILE = f"{SQL_PATH}\\mapping_source.json"
 DESTINATION_FILE = f"{SQL_PATH}\\mapping_destination.json"
 TRUNCATE_FILE = f"{SQL_PATH}\\destination\\db_tables__TRUNCATE.sql"
-
-logging.basicConfig(
-    filename=f"{LOGS_PATH}\\app.log",
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
 
 
 def main():
@@ -44,7 +37,7 @@ def main():
         create_pg_tables(DESTINATION_FILE)
 
     # execute truncate queries in destination
-    # truncate_pg_tables(TRUNCATE_FILE)
+    truncate_pg_tables(TRUNCATE_FILE)
     
     # execute select queries to get source data
     source_data = get_source_data(SOURCE_FILE)
