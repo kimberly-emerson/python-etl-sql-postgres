30666a2c30bf488778cd127224f85c3747bb1ad3
kimberly-emerson <kimberly.emerson@outlook.com>
2025-08-14 16:45:31 -0500

    Rename and update postgres.py -> postgresql.py

---
+++ src/db/postgresql.py
@@ -0,0 +1,148 @@
+"""
+tba
+"""
+import os
+import logging
+from psycopg2 import connect, DatabaseError, IntegrityError, Error
+from psycopg2.extras import execute_batch
+from utils.file_handler import read_query_from_file
+
+DESTINATION_PATH = f"{os.getenv("SQL_PATH")}\\destination"
+
+
+def execute_pg_query(conn, query: str, values: list = []):
+    """
+    tba
+    """
+    success = False
+
+    if conn is None:
+        # 
+        conn = set_pg_connection()
+    
+    cursor = conn.cursor()
+
+    try:
+
+        if values:
+            # execute insert query if values list injected
+            execute_batch(cursor, query, values, page_size=100)
+
+        else:
+            # execute other queries if no values list injected
+            cursor.execute(query)
+    
+        logging.info(f"SUCCESS: {query}")
+
+        conn.commit()
+
+        cursor.close()
+
+    except DatabaseError as error:
+        if conn:
+            conn.rollback()
+            logging.error(error)
+
+    except IntegrityError as error:
+        if conn:
+            conn.rollback()
+            logging.error(error)
+
+    except ValueError as error:
+        if conn:
+            conn.rollback()
+            logging.error(error)
+    
+    except SyntaxError as error:
+        if conn:
+            conn.rollback()
+            logging.error(error)
+    
+    except TypeError as error:
+        if conn:
+            conn.rollback()
+            logging.error(error)
+
+    except Error as error:
+        if conn:
+            conn.rollback() 
+            logging.error(error)
+
+    finally:
+        if conn:
+            conn.close()
+
+
+def set_pg_connection(database: str = None):
+    """
+    tba
+    """
+
+    conn = None
+
+    match database:
+        case "aw_sales":
+            conn = connect(
+                host=os.getenv("POSTGRESQL_HOSTNAME"),
+                port=os.getenv("POSTGRESQL_PORT"),
+                database=os.getenv("AW_SALES_DB_NAME"),
+                user=os.getenv("POSTGRES_DB_USERNAME"),
+                password=os.getenv("POSTGRES_DB_PASSWORD"),
+            )
+            conn.autocommit = True
+        case _:
+            conn = connect(
+                host=os.getenv("POSTGRESQL_HOSTNAME"),
+                port=os.getenv("POSTGRESQL_PORT"),
+                database=os.getenv("POSTGRES_DB_NAME"),
+                user=os.getenv("POSTGRES_DB_USERNAME"),
+                password=os.getenv("POSTGRES_DB_PASSWORD"),
+            )
+            conn.autocommit = True
+
+    return conn
+
+
+def build_pg_database():
+    """
+    tba
+    """
+
+    db_create_path = f"{DESTINATION_PATH}\\db_database__CREATE.sql"
+    db_grant_path = f"{DESTINATION_PATH}\\db_database__GRANT.sql"
+    role_create_path = f"{DESTINATION_PATH}\\db_role__CREATE.sql"
+    schemas_create_path = f"{DESTINATION_PATH}\\db_schemas__CREATE.sql"
+    role_grant_path = f"{DESTINATION_PATH}\\db_role__GRANT.sql"
+
+    # create database
+    pg_build(path=db_create_path, database=None)
+    # create role
+    pg_build(path=role_create_path, database=None)
+    # grant database permissions
+    pg_build(path=db_grant_path, database=None)
+    # create database schemas
+    pg_build(path=schemas_create_path, database="aw_sales")
+    # grant permissions on database tables and schemas
+    pg_build(path=role_grant_path, database="aw_sales")
+
+    logging.info("SUCCESS: Database build completed.")
+
+
+def pg_build(path: str = None, database: str = None):
+    """
+    tba
+    """
+    success: bool = False
+
+    try:
+        
+        query = read_query_from_file(path)
+        
+        conn = set_pg_connection(database)
+        
+        execute_pg_query(conn, query)
+    
+    except Exception as e:  # pylint: disable=broad-except
+        logging.error(e)
+
+    return success
