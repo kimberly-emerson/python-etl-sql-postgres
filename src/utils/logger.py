b390ee91fb56396c2dca768ed75a90eeff60c5a2
kimberly-emerson <kimberly.emerson@outlook.com>
2025-08-14 17:52:53 -0500

    Add logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,22 @@
+import os
+import logging
+
+LOGS_PATH = os.getenv("LOGS_PATH")
+
+
+logging.basicConfig(
+    filename=f"{LOGS_PATH}\\app.log",
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
