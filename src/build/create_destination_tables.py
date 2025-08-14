e4a40c9f6d5f572ac2aeec37753450dd076b1f70
kimberly-emerson <kimberly.emerson@outlook.com>
2025-08-14 16:48:52 -0500

    Add build folder and files

---
+++ src/build/create_destination_tables.py
@@ -0,0 +1,42 @@
+"""
+tba
+"""
+
+import os
+import logging
+from db.postgresql import execute_pg_query, set_pg_connection
+from utils.file_handler import get_query_list_from_file, read_query_from_file
+from utils.validation import validate_list
+
+DESTINATION_PATH = f"{os.getenv("SQL_PATH")}\\destination"
+
+
+def create_pg_tables(file: str=None):
+    
+    try:
+        queries = read_query_from_file(file)
+        
+        if queries[0]:
+            query_list = get_query_list_from_file("destination_query_create",queries[1])
+
+            for query in query_list[1]:
+
+                file = f"{DESTINATION_PATH}\\{query}"
+                
+                query = read_query_from_file(file)
+                
+                if not query:
+                    logging.warning(f"{file} does not return a query.")
+                    raise FileNotFoundError
+                
+                conn = set_pg_connection("aw_sales")
+                
+                execute_pg_query(conn,query=query)
+                
+                print(f"{file[49:len(file)]} executed.")
+
+    except FileNotFoundError as error:
+        logging.error(error)
+
+    except Exception as e:  # pylint: disable=broad-except
+        logging.error(e)
---
+++ src/build/truncate_destination_tables.py
@@ -0,0 +1,33 @@
+"""
+tba
+"""
+
+import logging
+from db.postgresql import execute_pg_query, set_pg_connection
+from utils.file_handler import read_query_from_file
+
+
+def truncate_pg_tables(file: str):
+    
+    try:
+        # read queries from truncate file
+        query = read_query_from_file(file)
+
+        # test if query is not empty        
+        if not query:
+            logging.warning(f"{file} does not return a query.")
+            raise FileNotFoundError
+        
+        # set database connection
+        conn = set_pg_connection("aw_sales")
+        
+        # execute queries in file
+        execute_pg_query(conn, query=query)
+        
+        logging.info(f"{file[49:len(file)]} executed.")
+
+    except FileNotFoundError as error:
+        logging.error(error)
+
+    except Exception as e:  # pylint: disable=broad-except
+        logging.error(e)
