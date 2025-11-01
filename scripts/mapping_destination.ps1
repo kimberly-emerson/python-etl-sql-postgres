13a538c1d742e7a4f424067011c00e17aab68483
kimberly-emerson <kimberly.emerson@outlook.com>
2025-11-01 16:35:05 -0500

    Removed Powershell Mapping Scripts

--- scripts/mapping_destination.ps1
+++
@@ -1,4 +0,0 @@
-$csvData = Import-Csv -Path "C:\demos\python-etl-sql-postgres\sql\mapping.csv"
-$csvData = $csvData | Where-Object Is_App_Table -eq 1 | Select-Object destination_query_create, destination_query_insert, table_id, execution_order | Sort-Object execution_order, destination_query_insert
-$jsonData = $csvData | ConvertTo-Json
-$jsonData | Set-Content -Path "C:\demos\python-etl-sql-postgres\sql\mapping_destination.json"
